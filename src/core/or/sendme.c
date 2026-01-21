41193df5346d13bf29f1d302639904f787a9f0d0
Roger Dingledine <arma@torproject.org>
2026-01-20 22:31:14 -0500

    sendme-version-0 dir fetches don't count toward geoip

--- src/core/or/sendme.c
+++ src/core/or/sendme.c
@@ -157,7 +157,7 @@ cell_version_can_be_handled(uint8_t cell_version)
  * send/recv cells on a circuit. If the SENDME is invalid, the circuit should
  * be marked for close by the caller. */
 STATIC bool
-sendme_is_valid(const circuit_t *circ, const uint8_t *cell_payload,
+sendme_is_valid(circuit_t *circ, const uint8_t *cell_payload,
                 size_t cell_payload_len)
 {
   uint8_t cell_version;
@@ -183,10 +183,13 @@ sendme_is_valid(const circuit_t *circ, const uint8_t *cell_payload,
 
   /* Validate that we can handle this cell version. */
   if (CIRCUIT_IS_ORCIRC(circ) &&
-      CONST_TO_OR_CIRCUIT(circ)->used_legacy_circuit_handshake &&
+      TO_OR_CIRCUIT(circ)->used_legacy_circuit_handshake &&
       cell_version == 0) {
     /* exception, allow v0 sendmes on circuits made with CREATE_FAST */
     log_info(LD_CIRC, "Permitting sendme version 0 on legacy circuit.");
+    /* Record this choice on the circuit, so we can avoid counting
+     * directory fetches on this circuit toward our geoip stats. */
+    TO_OR_CIRCUIT(circ)->used_obsolete_sendme = 1;
   } else if (!cell_version_can_be_handled(cell_version)) {
     goto invalid;
   }
--- src/feature/dircache/dirserv.c
+++ src/feature/dircache/dirserv.c
@@ -779,10 +779,15 @@ connection_dirserv_flushed_some(dir_connection_t *conn)
     tor_compress_free(conn->compress_state);
     conn->compress_state = NULL;
   }
+
+  /* only count networkstatus serves as successful when the spool runs dry */
   if (conn->should_count_geoip_when_finished) {
-    /* only count successfully networkstatus serves when the spool runs dry */
     tor_addr_t addr;
-    if (tor_addr_parse(&addr, (TO_CONN(conn))->address) >= 0) {
+    /* but as a special case, check if conn is on a circuit that used a
+     * version-0 sendme (bugs 41191 and 41192), because we don't want to
+     * count clients that should exit after they receive our consensus. */
+    if (!connection_dir_used_obsolete_sendme(conn) &&
+        tor_addr_parse(&addr, (TO_CONN(conn))->address) >= 0) {
       geoip_note_client_seen(GEOIP_CLIENT_NETWORKSTATUS,
                              &addr, NULL,
                              time(NULL));
